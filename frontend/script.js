031a96b80ab515c81b62eea5215d4689ae48abf2
chamanos <chamano.sr@gmail.com>
2023-11-10 09:17:29 +0200

    fixing layout

--- frontend/script.js
+++ frontend/script.js
@@ -1,24 +1,4 @@
 
-// function Dropdown(event){
-// const dynamic_id = event.replace("dropdown","collapse");
-// // const getElement = document.getElementById("dropdown_1");
-// const dynamic_collapseID = document.getElementById(dynamic_id).nextElementSibling.firstChild;
-// console.log("nextSiblingID" + dynamic_collapseID)
-// const getCollapse = document.getElementById("collapse_1");
-
-// dynamic_id.addEventListener("click",()=>{
-//     console.log(getCollapse)
-//     getCollapse.classList.toggle("show");
-
-//     if(document.getElementById(dynamic_id).nextElementSibling.classList == "block"){
-//         document.querySelector(".span-down").style.display = "block";
-//         document.querySelector(".span").style.display = "none";
-
-//     }
-// })
-
-// }
-
 function Dropdown(dropdownId) {
   
     const collapseId = dropdownId.replace("dropdown_", "collapse_");
@@ -41,4 +21,6 @@ function Dropdown(dropdownId) {
       arrowDown.style.display = "block";
     }
   }
+  
+
   
